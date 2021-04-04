46b700616d7b44a3081bbc7a6dcbdc1aa6a4af97
allentom <qweasdzxczse2@gmail.com>
2021-04-04 21:25:20 +0800

    update

--- webpack/react.webpack.js
+++ webpack/react.webpack.js
@@ -34,6 +34,9 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      { test: /\.tsx?$/, loader: "ts-loader",
+
+      }
     ]
   },
   devServer: {
